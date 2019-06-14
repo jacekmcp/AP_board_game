010593b81fa0373ed06c8b99642f00969e2255b5
Mateusz Wasilewski <matteuzs@o2.pl>
2019-06-14 14:47:47 +0100

    666 vol2

--- src/sample/View/Chuj.java
+++
@@ -1,9 +0,0 @@
-package sample.View;
-
-import javafx.application.Application;
-
-public class Chuj {
-    public static void main(String[] chuje){
-        BoardSetUp boardSetUp = new BoardSetUp();
-    }
-}
